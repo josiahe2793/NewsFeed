f4f36eee54994c5f981b0a43211cf78966dd648b
Josiah E <josiahmelbey@gmail.com>
2020-08-09 11:07:11 -0400

    API switch from NewsAPI to NYTAPI

--- app.js
+++ app.js
@@ -1,53 +1,51 @@
-$(document).ready(function(){
-    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=ba4ab04ba426466ebc8b7bc39b149e86";
+$(document).ready(function () {
+  let url =
+    "https://api.nytimes.com/svc/mostpopular/v2/shared/1/facebook.json?api-key=Dsak6ljecPclOkCgEeAdB1dmdr1bEqAC";
 
-    $.ajax({
-        url:url,
-        method:"GET",
-        dataType:"Json",
+  $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "JSON",
 
-         success: function(news){
-            let output = "";
-            let latestNews = news.articles;
-            let post_limit = 3;
-            let materializeColWidth = 12/post_limit;
+    success: function (newsdata) {
+      let output = "";
+      let latestNews = newsdata.results;
 
-            for(var i in latestNews){
-                output += `
-                     <div class="col l${materializeColWidth} m6 s12">
-                     <div class="card medium hoverable">
-                       <div class="card-image">
-                         <img src="${latestNews[i].urlToImage}" class="responsive-img"/>
-                       </div>
-                       <div class="card-content">
-                       <span class="card-title activator">
-                           <i class="material-icons right">more_vert</i>
-                       </span>
-                      <h6>${latestNews[i].title}</h6>
-                       </div>
+      for (var i in latestNews) {
+        output += `
+          <div class="col l4 m6 s12">
+          <div class="card medium hoverable">
+            <div class="card-image">
+              <img style="height: auto; width:auto;" src= "${latestNews[i].media[0]}" class = "responsive-img"
+ alt = "${latestNews[i].media}" >
+            </div>
+            <div class="card-content">
+              <span class="card-title activator"><i class="material-icons right">more_vert</i></span>
+              <h6 class="truncate">Title: <a href="${latestNews[i].url}" title="${latestNews[i].title}">${latestNews[i].title}</a></h6>
+              <p><b>Author</b>: ${latestNews[i].byline} </p>
+              <p><b>News source</b>: ${latestNews[i].source} </p>
+              <p><b>Published Date</b>: ${latestNews[i].published_date} </p>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title"><i class="material-icons right">close</i></span>
+              < p> <b> Description </b>: ${latestNews[i].abstract}</>
+            </div>
+            <div class="card-action">
+              <a href="${latestNews[i].url}" target="_blank" class="btn">Read More</a>
+            </div>
+           </div>
+          </div>
+        `;
+      }
 
-                       <div class="card-reveal">
-                        <span class="card-title activator">
-                           <i class="material-icons right">close</i>
-                       </span>
-                            <p>${latestNews[i].description}</p>
-                       </div>
+      if (output !== "") {
+        $("#newsResults").html(output);
+      }
+    },
 
-                       <div class="card-action">
-                          <a href="${latestNews[i].url}" class="btn" target="_blank">Read More</a>
-                       </div>
-                      </div>
-                    </div>`;
-            }
-
-            if(output !==""){
-                $("#newsResults").html(output);
-            }
-        },
-
-        error: function(){
-            $("#newsResults").html("some error occured");
-
-        }
-    })
+    error: function () {
+      let errorMsg = `<div class="errorMsg center">Some error occured</div>`;
+      $("#newsResults").html(errorMsg);
+    },
+  });
 });
